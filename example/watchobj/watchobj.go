e05392722411c3dfd2d8465d5d0cabd476e53bde
Naplesoul <shenwhang@outlook.com>
2022-04-26 14:29:19 +0800

    [improve]: add a simple test in watchobj example

--- example/watchobj/watchobj.go
+++ example/watchobj/watchobj.go
@@ -3,17 +3,28 @@ package main
 import (
 	"Cubernetes/pkg/watchobj"
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
 )
+
+func test() {
+	time.Sleep(time.Second)
+	for {
+		targetUrl := "http://127.0.0.1:8080/apis/pod/hello:e0a77a11-f736-4f5f-934e-f1f0a3c39172"
+		payload := strings.NewReader("{\"kind\":\"pod\",\"apiVersion\":\"3\",\"metadata\":{\"name\":\"hello\",\"uid\":\"hello:e0a77a11-f736-4f5f-934e-f1f0a3c39172\"},\"spec\":{\"containers\":null},\"status\":{}}")
+		req, _ := http.NewRequest("PUT", targetUrl, payload)
+		req.Header.Add("Content-Type", "application/json")
+		_, _ = http.DefaultClient.Do(req)
+		time.Sleep(time.Millisecond)
+	}
+}
 
 // simple example of use
 func main() {
 	ch, cancel := watchobj.WatchObj("/apis/watch/pod/hello:e0a77a11-f736-4f5f-934e-f1f0a3c39172")
-	i := 1
+	go test()
 	for str := range ch {
-		if i > 5 {
-			break
-		}
-		i++
 		fmt.Println(str)
 	}
 	// cancel watching
