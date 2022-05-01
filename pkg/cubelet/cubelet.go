157792ef705e7b264f6600e07d8f0a919857f117
lee <1179658820@qq.com>
2022-05-01 14:47:37 +0800

    [feat]: finish syncloop, wait for test

--- pkg/cubelet/cubelet.go
+++ pkg/cubelet/cubelet.go
@@ -1,7 +1,10 @@
 package cubelet
 
 import (
+	"Cubernetes/pkg/cubelet/container"
 	cuberuntime "Cubernetes/pkg/cubelet/cuberuntime"
+	"Cubernetes/pkg/watchobj"
+	"log"
 )
 
 type Cubelet struct {
@@ -9,9 +12,37 @@ type Cubelet struct {
 }
 
 func (cl *Cubelet) syncLoop() {
+	ch, cancel := watchobj.WatchPods()
+	defer cancel()
 
+	for podEvent := range ch {
+		switch podEvent.EType {
+		case watchobj.EVENT_PUT:
+			err := cl.runtime.SyncPod(&podEvent.Pod, &container.PodStatus{})
+			if err != nil {
+				return
+			}
+		case watchobj.EVENT_DELETE:
+			err := cl.runtime.KillPod(podEvent.Pod.UID)
+			if err != nil {
+				return
+			}
+		default:
+			log.Panic("Unsupported type in watch pod.")
+		}
+	}
 }
 
 func (cl *Cubelet) Run() {
+	if cl.runtime == nil {
+		runtime, err := cuberuntime.NewCubeRuntimeManager()
+		if err != nil {
+			panic(err)
+		}
 
+		cl.runtime = runtime
+	}
+
+	defer cl.runtime.Close()
+	cl.syncLoop()
 }
