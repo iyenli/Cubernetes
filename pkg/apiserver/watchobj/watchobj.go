c1873291c1312370ee03d38605ea59bc8741f7cf
Naplesoul <shenwhang@outlook.com>
2022-05-01 18:17:05 +0800

    [feat]: add crud apis, add watch apis for service

--- pkg/apiserver/watchobj/watchobj.go
+++ pkg/apiserver/watchobj/watchobj.go
@@ -110,6 +110,40 @@ func createPodWatch(url string) (chan PodEvent, context.CancelFunc, error) {
 	}, nil
 }
 
+func createServiceWatch(url string) (chan ServiceEvent, context.CancelFunc, error) {
+	ch := make(chan ServiceEvent)
+	var closed int32 = 0
+	closeChan := func() {
+		swapped := atomic.CompareAndSwapInt32(&closed, 0, 1)
+		if swapped {
+			log.Println("closing ServiceEvent channel")
+			close(ch)
+		}
+	}
+	stop, err := postWatch(url, closeChan, func(e ObjEvent) {
+		var serviceEvent ServiceEvent
+		serviceEvent.EType = e.EType
+		switch e.EType {
+		case EVENT_PUT:
+			err := json.Unmarshal([]byte(e.Object), &serviceEvent.Service)
+			if err != nil {
+				log.Println("fail to parse Service in ServiceEvent")
+				return
+			}
+		case EVENT_DELETE:
+			serviceEvent.Service.UID = e.Path[len("/apis/service/"):]
+		}
+		ch <- serviceEvent
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	return ch, func() {
+		closeChan()
+		stop()
+	}, nil
+}
+
 func WatchPod(UID string) (chan PodEvent, func(), error) {
 	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/watch/pod/" + UID
 	return createPodWatch(url)
@@ -121,4 +155,17 @@ func WatchPod(UID string) (chan PodEvent, func(), error) {
 func WatchPods() (chan PodEvent, func(), error) {
 	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/watch/pods"
 	return createPodWatch(url)
+}
+
+func WatchService(UID string) (chan ServiceEvent, func(), error) {
+	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/watch/service/" + UID
+	return createServiceWatch(url)
+}
+
+// WatchServices
+// if err != nil, chan and cancel() will be nil
+// if you call cancel() or connection failed, channel will be closed
+func WatchServices() (chan ServiceEvent, func(), error) {
+	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/watch/services"
+	return createServiceWatch(url)
 }
