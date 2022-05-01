6ec4faee26532d573e2d6e8be1420ea9dd0c562d
lee <1179658820@qq.com>
2022-05-01 16:21:46 +0800

    [test]: add cubelet test, wait for execute

---
+++ pkg/cubelet/testing/addpod.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"Cubernetes/pkg/watchobj"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func test() {
+	time.Sleep(time.Second)
+	for {
+		targetUrl := "nginx:foo-bar-meaningless-uid"
+		payload := strings.NewReader(`
+{
+  "kind": "Pod",
+  "apiVersion": "wahtever/v1",
+  "metadata": {
+    "name": "hello",
+    "uid": "nginx:foo-bar-meaningless-uid"
+  },
+  "spec": {
+    "containers": [
+      {
+        "name": "foo-nginx",
+        "image": "nginx",
+        "ports": [
+          {
+            "containerPort": 8080,
+            "hostPort": 80
+          }
+        ],
+        "volumeMounts": [
+          {
+            "name": "conf",
+            "mountPath": "/etc/nginx/nginx.conf"
+          },
+          {
+            "name": "html",
+            "mountPath": "/www"
+          }
+        ]
+      }
+    ],
+    "volumes": [
+      {
+        "name": "conf",
+        "hostPath": "/home/lee/CloudOS/test/nginx.conf/nginx.conf"
+      },
+      {
+        "name": "html",
+        "hostPath": "/home/lee/CloudOS/test/www"
+      }
+    ]
+  }
+}
+`)
+		req, _ := http.NewRequest("PUT", targetUrl, payload)
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := http.DefaultClient.Do(req)
+
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(resp.Status)
+		time.Sleep(15 * time.Second)
+	}
+}
+
+// simple example of use
+func main() {
+	ch, cancel := watchobj.WatchPods()
+	go test()
+	for podEvent := range ch {
+		fmt.Println(podEvent)
+	}
+	// cancel watching
+	cancel()
+}
