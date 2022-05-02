e0ded14129edf72192d3838172fe132feee10fe8
Zhenyu Yang <jolynefr@sjtu.edu.cn>
2022-05-02 21:22:49 +0800

    [feat] skeletion of rs-controller, waiting for apiserver

--- pkg/object/types.go
+++ pkg/object/types.go
@@ -13,8 +13,37 @@ type PodSpec struct {
 	Volumes    []Volume    `json:"volumes,omitempty"`
 }
 
+// PodPhase is a label for the condition of a pod at the current time.
+type PodPhase string
+
+// These are the valid statuses of pods.
+const (
+	// PodCreated means that API Pod object was created by API Server
+	PodCreated PodPhase = "Created"
+	// PodBound means that scheduler bind this pod to a node
+	PodBound PodPhase = "Bound"
+	// PodAccepted means cubelet has accepted this pod to run
+	PodAccepted PodPhase = "Accepted"
+	// PodPending means the pod has been accepted by the system, but one or more of the containers
+	// has not been started. This includes time before being bound to a node, as well as time spent
+	// pulling images onto the host.
+	PodPending PodPhase = "Pending"
+	// PodRunning means the pod has been bound to a node and all of the containers have been started.
+	// At least one container is still running or is in the process of being restarted.
+	PodRunning PodPhase = "Running"
+	// PodSucceeded means that all containers in the pod have voluntarily terminated
+	// with a container exit code of 0, and the system is not going to restart any of these containers.
+	PodSucceeded PodPhase = "Succeeded"
+	// PodFailed means that all containers in the pod have terminated, and at least one container has
+	// terminated in a failure (exited with a non-zero exit code or was stopped by the system).
+	PodFailed PodPhase = "Failed"
+	// PodUnknown means that for some reason the state of the pod could not be obtained, typically due
+	// to an error in communicating with the host of the pod.
+	PodUnknown PodPhase = "Unknown"
+)
+
 type PodStatus struct {
-	// reserved for later use
+	Phase PodPhase `json:"phase,omitempty"`
 }
 
 type Container struct {
