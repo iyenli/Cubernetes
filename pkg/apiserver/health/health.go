67c364f9668d61c0ce895bdc6ff5e81710b95080
Naplesoul <shenwhang@outlook.com>
2022-05-18 23:46:36 +0800

    [feat]: automatic retry after heartbeat is lost

--- pkg/apiserver/health/health.go
+++ pkg/apiserver/health/health.go
@@ -2,7 +2,6 @@ package health
 
 import (
 	cubeconfig "Cubernetes/config"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -10,9 +9,7 @@ import (
 func CheckApiServerHealth() bool {
 	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/health"
 	resp, err := http.Get(url)
-
 	if err != nil {
-		log.Println("fail to send http get request")
 		return false
 	}
 	defer func() { _ = resp.Body.Close() }()
