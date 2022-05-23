40cc3fd370f8dee088dc96f0b2246bf44256cdca
lee <1179658820@qq.com>
2022-05-23 18:57:57 +0800

    [feat]: add reader and fix interface

--- pkg/utils/kafka/kafka.go
+++ pkg/utils/kafka/kafka.go
@@ -8,6 +8,16 @@ import (
 )
 import "github.com/segmentio/kafka-go"
 
+func NewKafkaClient(host string) *kafka.Conn {
+	address := net.JoinHostPort(host, options.KafkaPort)
+	conn, err := kafka.Dial("tcp", address)
+	if err != nil {
+		log.Println("[Error]: Get client failed")
+		return nil
+	}
+	return conn
+}
+
 func NewKafkaClientByTopic(topic string, partition int, host string) *kafka.Conn {
 	address := net.JoinHostPort(host, options.KafkaPort)
 	conn, err := kafka.DialLeader(context.Background(), "tcp", address, topic, partition)
