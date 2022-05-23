bc799f6c5e1baec7dfadcfc37af54bea65242c43
Naplesoul <shenwhang@outlook.com>
2022-05-23 08:43:16 +0800

    [improve]: better log in crud apis

--- pkg/apiserver/jobfile/jobfile.go
+++ pkg/apiserver/jobfile/jobfile.go
@@ -17,24 +17,25 @@ func GetJobFile(JobUID string, filename string) error {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		log.Println("fail to send http get request")
+		log.Println("fail to send http get request, err: ", err)
 		return err
 	}
 	defer func() { _ = resp.Body.Close() }()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("fail to read http get response body")
+		log.Println("fail to read http get response body, err: ", err)
 		return err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("job file not found")
+		log.Printf("HTTP GET NOT OK when getting job file, status code: %v, server response: %s\n", resp.StatusCode, string(body))
+		return errors.New(string(body))
 	}
 
 	err = ioutil.WriteFile(filename, body, 0777)
 	if err != nil {
-		log.Println("fail to write into file: ", filename)
+		log.Println("fail to write into file: ", filename, ", err: ", err)
 		return err
 	}
 
@@ -44,7 +45,7 @@ func GetJobFile(JobUID string, filename string) error {
 func PostJobFile(JobUID string, filename string) error {
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Println("fail to read file: ", filename)
+		log.Println("fail to read file: ", filename, ", err: ", err)
 		return err
 	}
 
@@ -71,15 +72,22 @@ func PostJobFile(JobUID string, filename string) error {
 	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/gpuJob/file/" + JobUID
 	resp, err := http.Post(url, writer.FormDataContentType(), &body)
 
-	if err != nil || resp == nil {
-		log.Println("fail to send http post request")
+	if err != nil {
+		log.Println("fail to send http post request, err: ", err)
 		return err
 	}
 
 	defer func() { _ = resp.Body.Close() }()
 
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("fail to read http post response body, err: ", err)
+		return err
+	}
+
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("fail to upload job file")
+		log.Printf("HTTP POST NOT OK when posting job file, status code: %v, server response: %s\n", resp.StatusCode, string(respBody))
+		return errors.New(string(respBody))
 	}
 
 	return nil
@@ -90,19 +98,20 @@ func GetJobOutput(JobUID string) (string, error) {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		log.Println("fail to send http get request")
+		log.Println("fail to send http get request, err: ", err)
 		return "", err
 	}
 	defer func() { _ = resp.Body.Close() }()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("fail to read http get response body")
+		log.Println("fail to read http get response body, err: ", err)
 		return "", err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return "", errors.New("job not finished")
+		log.Printf("HTTP GET NOT OK when getting job output, status code: %v, server response: %s\n", resp.StatusCode, string(body))
+		return "", errors.New(string(body))
 	}
 
 	return string(body), nil
@@ -113,14 +122,21 @@ func PostJobOutput(JobUID string, output string) error {
 
 	resp, err := http.Post(url, "text/plain", strings.NewReader(output))
 	if err != nil || resp == nil {
-		log.Println("fail to send http post request")
+		log.Println("fail to send http post request, err: ", err)
 		return err
 	}
 
 	defer func() { _ = resp.Body.Close() }()
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("fail to read http post response body, err: ", err)
+		return err
+	}
+
 	if resp.StatusCode != http.StatusOK {
-		return errors.New("fail to post job output")
+		log.Printf("HTTP POST NOT OK when posting job output, status code: %v, server response: %s\n", resp.StatusCode, string(body))
+		return errors.New(string(body))
 	}
 
 	return nil
