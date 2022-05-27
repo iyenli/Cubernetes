509fba9aaaf9ced6bf7508a9f3343e7b49f190fb
lee <1179658820@qq.com>
2022-05-27 09:47:10 +0800

    [fix]: cancel omitempty

--- pkg/gateway/types/mqmsg.go
+++ pkg/gateway/types/mqmsg.go
@@ -2,12 +2,12 @@ package types
 
 type MQMessage struct {
 	RequestUID  string `json:"requestUID" yaml:"requestUID"`
-	TriggerPath string `json:"triggerPath,omitempty" yaml:"triggerPath,omitempty"`
+	TriggerPath string `json:"triggerPath" yaml:"triggerPath"`
 
-	ReturnTopic string `json:"returnTopic,omitempty" yaml:"returnTopic,omitempty"`
+	ReturnTopic string `json:"returnTopic" yaml:"returnTopic"`
 	ContentType string `json:"contentType" yaml:"contentType"`
 	StatusCode  string `json:"statusCode" yaml:"statusCode"`
 
-	Params  map[string]string `json:"params,omitempty" yaml:"params,omitempty"`
+	Params  map[string]string `json:"params" yaml:"params"`
 	Payload string            `json:"payload" yaml:"payload"`
 }
