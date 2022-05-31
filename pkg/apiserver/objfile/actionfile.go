f7eabe01fb7fa444dd45f64a2910aed1ee633a8d
Naplesoul <shenwhang@outlook.com>
2022-05-31 19:53:49 +0800

    [feat]: display action script when describing an action

--- pkg/apiserver/objfile/actionfile.go
+++ pkg/apiserver/objfile/actionfile.go
@@ -14,3 +14,13 @@ func PostActionFile(ScriptUID string, filename string) error {
 	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/action/file/" + ScriptUID
 	return postFile(url, filename)
 }
+
+func GetActionFileStr(ScriptUID string) (string, error) {
+	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/action/file/" + ScriptUID
+	return getFileStr(url)
+}
+
+func PostActionFileStr(ScriptUID string, content string) error {
+	url := "http://" + cubeconfig.APIServerIp + ":" + strconv.Itoa(cubeconfig.APIServerPort) + "/apis/action/file/" + ScriptUID
+	return postFileStr(url, content)
+}
